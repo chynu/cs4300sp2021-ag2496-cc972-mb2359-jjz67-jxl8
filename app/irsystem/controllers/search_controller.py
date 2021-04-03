7ed3979a41a5cc9c947dc9d2f378440ad827c267
jxl8 <jxl8@cornell.edu>
2021-04-02 21:44:02 -0400

    Updated project name and NetIDs

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Similar Singer"
+net_id = "Alyssa Gao (ag2496), Celine Choo (cc972), Mahak Bindal (mb2359), Jerilyn Zheng (jjz67), Jasper Liang (jxl8)"
 
 @irsystem.route('/', methods=['GET'])
 def search():
