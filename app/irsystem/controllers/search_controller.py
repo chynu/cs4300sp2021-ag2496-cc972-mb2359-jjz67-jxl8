4671bb9bf680e2ab614aecb3bd40bef49e1102dd
celine <celine.choo98@gmail.com>
2021-04-14 16:40:14 -0400

    Added pictures for sample results

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,9 +5,9 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 project_name = "Similar Singer"
 net_id = "Alyssa Gao (ag2496), Celine Choo (cc972), Mahak Bindal (mb2359), Jerilyn Zheng (jjz67), Jasper Liang (jxl8)"
 sample_data = [
-	{'artist_name': 'Amerie', 'description': 'Lorem ipsum'},
-	{'artist_name': 'Christina Aguilera', 'description': 'Lorem ipsum'},
-	{'artist_name': 'Rhianna', 'description': 'Lorem ipsum'}]
+	{'artist_name': 'Amerie', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/d/dc/Amerie_-_All_I_Have_album.jpg'},
+	{'artist_name': 'Christina Aguilera', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/0/07/Christinaaguilera-christinaaguilera.jpg'},
+	{'artist_name': 'Rhianna', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png'}]
 
 @irsystem.route('/', methods=['GET'])
 def search():
