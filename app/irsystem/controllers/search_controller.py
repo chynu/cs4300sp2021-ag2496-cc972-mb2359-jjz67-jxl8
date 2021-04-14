d797129bd96549a85a5f44106cfedd023af8ba90
celine <celine.choo98@gmail.com>
2021-04-14 16:32:58 -0400

    Added sample results to results page, added sidebar

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -4,6 +4,10 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Similar Singer"
 net_id = "Alyssa Gao (ag2496), Celine Choo (cc972), Mahak Bindal (mb2359), Jerilyn Zheng (jjz67), Jasper Liang (jxl8)"
+sample_data = [
+	{'artist_name': 'Amerie', 'description': 'Lorem ipsum'},
+	{'artist_name': 'Christina Aguilera', 'description': 'Lorem ipsum'},
+	{'artist_name': 'Rhianna', 'description': 'Lorem ipsum'}]
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -15,5 +19,5 @@ def search():
 	else:
 		output_message = "Your search: " + query
 		data = range(5)
-		return render_template('search_results.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+		return render_template('search_results.html', name=project_name, netid=net_id, output_message=output_message, data=sample_data)
 
