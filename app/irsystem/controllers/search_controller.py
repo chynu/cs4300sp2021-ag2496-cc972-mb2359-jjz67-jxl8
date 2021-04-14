680bf4581f2f0a8e740ac46e2ad9956574e69917
celine <celine.choo98@gmail.com>
2021-04-14 17:13:01 -0400

    Code cleanup

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -4,6 +4,8 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
 project_name = "Similar Singer"
 net_id = "Alyssa Gao (ag2496), Celine Choo (cc972), Mahak Bindal (mb2359), Jerilyn Zheng (jjz67), Jasper Liang (jxl8)"
+
+# TODO: Remove the `sample_data` variable once we are able to dynamically retrieve data. This is here for demo purposes.
 sample_data = [
 	{'artist_name': 'Amerie', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/d/dc/Amerie_-_All_I_Have_album.jpg'},
 	{'artist_name': 'Christina Aguilera', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/0/07/Christinaaguilera-christinaaguilera.jpg'},
@@ -18,6 +20,4 @@ def search():
 		return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 	else:
 		output_message = "Based on your query, " + query + ", we recommend..."
-		data = range(5)
 		return render_template('search_results.html', name=project_name, netid=net_id, output_message=output_message, data=sample_data)
-
