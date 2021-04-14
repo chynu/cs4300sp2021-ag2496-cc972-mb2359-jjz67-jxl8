fd4e81777d628d3be804d93d06ea9acc8e86e671
celine <celine.choo98@gmail.com>
2021-04-14 16:55:32 -0400

    Added search input on results page

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -17,7 +17,7 @@ def search():
 		output_message = ''
 		return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 	else:
-		output_message = "Your search: " + query
+		output_message = "Based on your query, " + query + ", we recommend..."
 		data = range(5)
 		return render_template('search_results.html', name=project_name, netid=net_id, output_message=output_message, data=sample_data)
 
