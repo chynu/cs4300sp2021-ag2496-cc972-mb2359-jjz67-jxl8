2b8bb917b3298c75ca0f164076bfe165b4175f66
Alyssa Gao <ag2496@cornell.edu>
2021-04-18 15:47:21 -0400

    Finished cosine similarity

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,12 +3,19 @@ from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import pandas as pd
 import numpy as np
+import zipfile
 
 project_name = "Similar Singer"
 net_id = "Alyssa Gao (ag2496), Celine Choo (cc972), Mahak Bindal (mb2359), Jerilyn Zheng (jjz67), Jasper Liang (jxl8)"
 
+with zipfile.ZipFile('lyrics_data/tfidf_svd.zip', 'r') as zip_ref:
+    zip_ref.extractall('lyrics_data')
 tf_idf = pd.read_csv('lyrics_data/tfidf_mat_uncompressed.csv')
 artist_details = pd.read_csv('compiled-w-songs_new.csv')
+
+artist_names = tf_idf.values[:,0]
+artist_name_to_index = {artist_names[i]: i for i in range(len(artist_names))}
+matrix = tf_idf.to_numpy()[:,1:]
 
 # TODO: Remove the `sample_data` variable once we are able to dynamically retrieve data. This is here for demo purposes.
 # sample_data = [
@@ -17,13 +24,23 @@ artist_details = pd.read_csv('compiled-w-songs_new.csv')
 # 	{'artist_name': 'Rhianna', 'description': 'Lorem ipsum', 'img_url': 'https://upload.wikimedia.org/wikipedia/en/3/32/Rihanna_-_Anti.png'}]
 
 def find_artist(artist_name, csv):
-    col = 1 if (csv.values[0, 0] == 0) else 0    # checking which column contains the list of artists
+    """ Returns index of [artist_name].
+    
+    Parameters: {artist_name: String}
+    Returns: Int
+    """
+    col = 1 if (csv.values[0, 0] == 0) else 0 # checking which column contains the list of artists
     try:
         return np.argwhere(csv.values[:, col] == artist_name)[0][0]
     except:
         return -1
 
 def get_artist_description(artist_name):
+    """ Returns description of [artist_name].
+    
+    Parameters: {artist_name: String}
+    Returns: String
+    """
     try:
         followers = artist_details['followers'][find_artist(artist_name, artist_details)]
         return artist_name + " has " + str(followers) + " followers on Spotify."
@@ -31,28 +48,99 @@ def get_artist_description(artist_name):
         return "Couldn't find additional details on " + artist_name + ". "
 
 def get_artist_photo(artist_name):
+    """ Returns url of [artist_name]'s photo.
+    
+    Parameters: {artist_name: String}
+    Returns: String
+    """
     try:
         return artist_details['Image URL'][find_artist(artist_name, artist_details)]
     except:
         return "https://www.pngitem.com/pimgs/m/148-1487614_spotify-logo-small-spotify-logo-transparent-hd-png.png"
 
-def get_rec_artists(query, ling_desc, disliked_artist):
-    rec_artists = []
-    idx = find_artist(query, tf_idf)
-    if (idx == -1):
-        return []
+def rocchio_update(query, query_obj, input_doc_mat=matrix, \
+        artist_name_to_index=artist_name_to_index,a=.3, b=.3, c=.8):
+    """ Returns a vector representing the modified query vector.
 
-    # TODO: replace with recommended artists retrieval
-    i = 0
-    while (i < 10):
-        idx += 1
-        rec_artist = tf_idf.values[idx % len(tf_idf)][0]
-        if (rec_artist != disliked_artist):
-            rec_artists.append(rec_artist)
-            i += 1
-    return rec_artists
+    Note:
+        Be sure to handle the cases where relevant and irrelevant are empty lists.
+        
+    Params: {query: Int (index of artist being queried for),
+             query_obj: Dict (storing the names of relevant and irrelevant artists for query),
+             input_doc_mat: Numpy Array,
+             artist_name_to_index: Dict,
+             a,b,c: floats (weighting of the original query, relevant artists,
+                and irrelevant artists, respectively)}
+    Returns: np.ndarray
+    """
+    q = input_doc_mat[query]
+    dimension = len(q)
+    rel_d, irrel_d = np.zeros(dimension), np.zeros(dimension)
+    relevant, irrelevant = query_obj['relevant_artists'], query_obj['irrelevant_artists']
+    len_rel, len_irrel = len(relevant), len(irrelevant)
+    
+    for r in range(len_rel): # Get centroid of relevant artists
+        artist = relevant[r]
+        rel_d = rel_d + input_doc_mat[artist_name_to_index[artist]]
+        
+    for i in range(len_irrel): # Get centroid of irrelevant artists
+        artist = irrelevant[i]
+        irrel_d = irrel_d + input_doc_mat[artist_name_to_index[artist]]
+    
+    rocchio = a * q
+    if len_rel > 0:
+        rocchio += b * rel_d / len_rel
+    if len_irrel > 0:
+        rocchio -= c * irrel_d / len_irrel
+        
+    return np.clip(rocchio, 0, None)
+
+def cosine_similarity(query_vec, tfidf_mat=matrix, artist_names=artist_names):
+    """ Returns ranking of artist names using cosine similarity with query_vec.
+    
+    Params: {query_vec: np.ndarray - (k,)
+             tfidf_mat: np.ndarray - d x k (where d is number of documents/artists,
+                and rows are normalized)
+             artist_names: List}
+    Returns: List
+    """
+    scores = tfidf_mat.dot(query_vec)
+    ranking = np.argsort(scores)
+    
+    return [artist_names[i] for i in ranking[::-1]]
+
+def get_rec_artists(query, ling_desc, disliked_artist, artist_name_to_index=artist_name_to_index):
+    """ Returns list of recommended artists that are similar to [query] and dissimilar
+        to [disliked_artist].
+    
+    Parameters: {query: String (liked artist)
+                 ling_desc: String
+                 disliked_artist: String
+                 artist_name_to_index: Dict}
+    Returns: List
+    """
+    if query not in artist_name_to_index:
+        return []
+    idx = artist_name_to_index[query]
+    
+    query_obj = {
+        'relevant_artists': [query] if query else [],
+        'irrelevant_artists': [disliked_artist] if disliked_artist else []
+    }
+    query_vec = rocchio_update(idx, query_obj)
+    
+    artist_ranking = list(filter(lambda x: x != query, cosine_similarity(query_vec)))
+    return artist_ranking[:10]
 
 def get_results(query, ling_desc, disliked_artist):
+    """ Returns list of recommended artists who are similar to [query] and dissimilar
+        to [disliked_artist] along with their name, description and photo.
+    
+    Parameters: {query: String (liked artist)
+                 ling_desc: String
+                 disliked_artist: String}
+    Returns: List
+    """
     data = []
     top_rec_artists = get_rec_artists(query, ling_desc, disliked_artist)
     if (top_rec_artists == []):
@@ -63,6 +151,7 @@ def get_results(query, ling_desc, disliked_artist):
 
 @irsystem.route('/', methods=['GET'])
 def search():
+    """ Returns UI rendering of results. """
     query = request.args.get('search')
     ling_desc = request.args.get('ling_desc')
     disliked_artist = request.args.get('disliked_artist')
