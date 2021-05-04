448209d30007928160436a35d63570017e48ae51
celine <celine.choo98@gmail.com>
2021-05-03 22:57:21 -0400

    hello fix

--- app/static/scripts/results.js
+++ app/static/scripts/results.js
@@ -12,3 +12,27 @@ $('.spotify.followers').on("click", (e) => {
        player.attr("data-active", "true");
    }
 });
+
+function shortenNumber(n) {
+    let s = "" + n;
+    let suffix = "";
+    let threes = Math.floor(s.length / 3);
+    if (threes == 0) {
+        suffix = "";
+    } else if (threes == 1) {
+        suffix = "K";
+    } else if (threes == 2) {
+        suffix = "M";
+    } else if (threes == 3) {
+        suffix = "T"
+    } else {
+        return n;
+    }
+    return Math.round(n/(Math.pow(10,3*threes))) + suffix;
+}
+
+$('.spotify.followers').each(function () {
+    console.log($(this));
+    let followers = parseInt($(this).attr("data-followers"));
+    $(this).append(shortenNumber(followers) + " followers");
+});
