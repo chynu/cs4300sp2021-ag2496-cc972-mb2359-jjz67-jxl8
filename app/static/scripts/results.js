263e786f7f547cf5b6fa77fbe91da80735f260ae
celine <celine.choo98@gmail.com>
2021-05-05 21:15:26 -0400

    Making spotify button more clear that it is clickable

--- app/static/scripts/results.js
+++ app/static/scripts/results.js
@@ -1,16 +1,20 @@
 $('.spotify.followers').on("click", (e) => {
    let artistBody = $(e.target).parent().parent();
    let player = artistBody.find(".spotify-player");
-   if (player.attr("data-active") == "true") {
+   toggleSpotifyPlayer(player);
+});
+
+function toggleSpotifyPlayer(playerObj) {
+    if (playerObj.attr("data-active") == "true") {
        // Close player
-       player.removeClass("expanded");
-       player.attr("data-active", "false");
+       playerObj.removeClass("expanded");
+       playerObj.attr("data-active", "false");
    } else {
        // Open player
-       player.addClass("expanded");
-       player.attr("data-active", "true");
+       playerObj.addClass("expanded");
+       playerObj.attr("data-active", "true");
    }
-});
+}
 
 function shortenNumber(n) {
     if (n >= 1000000) return internationalize(n/1000000) + "M";
